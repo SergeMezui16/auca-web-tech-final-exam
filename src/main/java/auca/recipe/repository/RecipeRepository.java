67b02ba3c25dc7c443528671bb3a3bbefd8c6799
sergemezui <sergemezui.dev@gmail.com>
2024-12-04 16:33:31 +0100

    Add method to fetch ingredients by recipe ID
    Introduce a custom query method in the RecipeRepository to retrieve ingredients based on a specific recipe ID. This enhances the ability to manage and query recipe-related data more efficiently within the application.

--- src/main/java/auca/recipe/repository/RecipeRepository.java
+++ src/main/java/auca/recipe/repository/RecipeRepository.java
@@ -1,9 +1,17 @@
 package auca.recipe.repository;
 
+import auca.recipe.entity.Ingredient;
 import auca.recipe.entity.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+
+    @Query("SELECT i FROM Ingredient i WHERE i.recipe.id = :recipeId")
+    List<Ingredient> getIngredientsById(@Param("recipeId") Long recipeId);
 }
