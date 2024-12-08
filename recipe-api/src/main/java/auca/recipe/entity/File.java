38809847630be7dbe9e46e3249868b12161a0397
sergemezui <sergemezui.dev@gmail.com>
2024-12-08 23:20:32 +0100

    Add File entity for storing file data
    Introduce a new File entity class with fields for name, type, and binary data. This class includes basic getter and setter methods and uses JPA annotations to map it as a database entity. This enables persistence of file metadata and its content in the database.

---
+++ recipe-api/src/main/java/auca/recipe/entity/File.java
@@ -0,0 +1,56 @@
+package auca.recipe.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+@Entity
+public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String type;
+
+    @Lob
+    private byte[] data;
+
+    public File() {
+    }
+
+    public File(String name, String type, byte[] data) {
+        this.name = name;
+        this.type = type;
+        this.data = data;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+}
