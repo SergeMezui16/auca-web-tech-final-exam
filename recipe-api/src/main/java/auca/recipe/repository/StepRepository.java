85c174bb77a385a98ee463ab0d6507659449e618
sergemezui <sergemezui.dev@gmail.com>
2024-12-08 21:45:13 +0100

    Add method to find steps by recipe ID in StepRepository
    Introduced a new method `findAllByRecipeId` in the StepRepository interface to retrieve all steps associated with a specific recipe ID. This enhancement improves querying efficiency and aligns with common repository usage patterns.

--- recipe-api/src/main/java/auca/recipe/repository/StepRepository.java
+++ recipe-api/src/main/java/auca/recipe/repository/StepRepository.java
@@ -4,7 +4,10 @@ import auca.recipe.entity.Step;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StepRepository extends JpaRepository<Step, Long> {
 
+    List<Step> findAllByRecipeId(Long id);
 }
