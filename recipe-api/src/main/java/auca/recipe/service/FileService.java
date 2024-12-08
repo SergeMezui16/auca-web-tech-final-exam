76aeed63e92eb49ad027f15330d2b58db34c9f79
sergemezui <sergemezui.dev@gmail.com>
2024-12-08 23:20:53 +0100

    Add FileService for file storage and retrieval
    Introduce a new FileService class to handle storing, retrieving, and listing files in the database. This service uses FileRepository for database operations and supports basic functionality such as saving a file, fetching a file by ID, and retrieving all files.

---
+++ recipe-api/src/main/java/auca/recipe/service/FileService.java
@@ -0,0 +1,39 @@
+package auca.recipe.service;
+
+import auca.recipe.entity.File;
+import auca.recipe.repository.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+@Service
+public class FileService {
+
+    FileRepository repository;
+
+    public FileService(@Autowired FileRepository repository) {
+        this.repository = repository;
+    }
+
+
+    public File store(MultipartFile file) throws IOException {
+        String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+        File FileDB = new File(fileName, file.getContentType(), file.getBytes());
+
+        return repository.save(FileDB);
+    }
+
+    public Optional<File> getFile(Long id) {
+        return repository.findById(id);
+    }
+
+    public Stream<File> getAllFiles() {
+        return repository.findAll().stream();
+    }
+}
