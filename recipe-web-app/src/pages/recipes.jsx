01f5e2b731bb8e2f01f39695b1dadf3368409754
sergemezui <sergemezui.dev@gmail.com>
2024-12-11 00:43:11 +0100

    Rename component and update grid layout responsiveness
    Renamed the `Recipes` component to `RecipePage` for clarity. Adjusted the grid layout to improve responsiveness across screen sizes, supporting better user experience on various devices.

--- recipe-web-app/src/pages/recipes.jsx
+++ recipe-web-app/src/pages/recipes.jsx
@@ -1,8 +1,8 @@
 import { RecipeCard } from "@/components/molecule/index.js";
 
-export default function Recipes() {
+export default function RecipePage() {
   return <div className="container mx-auto ">
-    <div className="grid grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 items-center justify-center mx-auto  gap-4 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
       {[1, 2, 3, 4, 5].map(a => <RecipeCard key={a}/>)}
     </div>
   </div>;
