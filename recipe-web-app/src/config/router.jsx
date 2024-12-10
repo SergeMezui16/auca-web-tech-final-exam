fb06175724e23acdaa4798face44dc8415937ab5
sergemezui <sergemezui.dev@gmail.com>
2024-12-11 00:41:46 +0100

    Update router config with nested routes and new pages
    Added `AppLayout` as a wrapper with nested routes for home, recipes, and recipe-related views. Introduced separate routes for login, register, new recipe creation, and editing. This change enhances organization and enables better navigation structure.

--- recipe-web-app/src/config/router.jsx
+++ recipe-web-app/src/config/router.jsx
@@ -1,9 +1,36 @@
 import { createBrowserRouter } from "react-router";
 import { RegisterPage } from "@/pages/register";
+import { LoginPage } from "@/pages/login.jsx";
+import HomePage from "@/pages/home.jsx";
+import RecipePage from "@/pages/recipes.jsx";
+import RecipeDetail from "@/pages/recipe.jsx";
+import { AppLayout } from "@/components/organism/app-layout.jsx";
+import { NewRecipePage } from "@/pages/new-recipe.jsx";
+import { RecipeEditPage } from "@/pages/recipe-edit.jsx";
 
 export const router = createBrowserRouter([
   {
     path: "/",
+    element: <AppLayout/>,
+    children: [
+      {index: true, element: <HomePage/>},
+      {path: 'new', element: <NewRecipePage />},
+      {
+        path: "recipes",
+        children: [
+          {index: true, element: <RecipePage/>},
+          {path: ":id", element: <RecipeDetail/>},
+          {path: ":id/edit", element: <RecipeEditPage/>},
+        ]
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <LoginPage/>
+  },
+  {
+    path: "/register",
     element: <RegisterPage/>
   }
 ]);
