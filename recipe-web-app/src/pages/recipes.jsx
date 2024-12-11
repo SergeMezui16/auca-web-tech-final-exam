c53d356c6496bd066abb660562cd4f8288080395
sergemezui <sergemezui.dev@gmail.com>
2024-12-11 12:34:53 +0100

    Fetch recipe data dynamically and handle loading state.
    Replaced static recipe card data with dynamic fetching using `useFetchQuery` from `/recipes` endpoint. Added a loading state to improve user experience while data is being retrieved. This ensures the page displays real-time recipe information.

--- recipe-web-app/src/pages/recipes.jsx
+++ recipe-web-app/src/pages/recipes.jsx
@@ -1,9 +1,15 @@
 import { RecipeCard } from "@/components/molecule/index.js";
+import { useFetchQuery } from "@/hooks/use-queries.js";
 
 export default function RecipePage() {
+  const {isPending, data} = useFetchQuery("/recipes");
+
+  if (isPending) return <div>Loading...</div>;
+
   return <div className="container mx-auto ">
-    <div className="grid grid-cols-1 items-center justify-center mx-auto  gap-4 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
-      {[1, 2, 3, 4, 5].map(a => <RecipeCard key={a}/>)}
+    <div
+      className="grid grid-cols-1 items-center justify-center mx-auto  gap-4 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+      {data.map(recipe => <RecipeCard recipe={recipe} key={recipe.id}/>)}
     </div>
   </div>;
 }
