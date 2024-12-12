26a2126b1b6fa70f357ade58ca35d6eea09cb19d
sergemezui <sergemezui.dev@gmail.com>
2024-12-13 00:10:59 +0100

    Add authentication handling to AppLayout component
    Introduced logic to check user authentication status within AppLayout. Redirects guests to the login page and displays a loading state for pending authentication. Incorporated `useAuth` and `useFetchQuery` hooks to manage auth flow effectively.

--- recipe-web-app/src/components/organism/app-layout.jsx
+++ recipe-web-app/src/components/organism/app-layout.jsx
@@ -1,11 +1,37 @@
 import { Header } from "@/components/organism/header";
 import { Footer } from "@/components/organism/footer";
-import { Outlet } from "react-router";
+import { Navigate, Outlet } from "react-router";
+import { AuthStatus, useAuth } from "@/hooks/use-auth.js";
+import { useFetchQuery } from "@/hooks/use-queries.js";
+import { useEffect } from "react";
 
 export const AppLayout = () => {
+  const {status, isPending} = useLogin()
+
+  if (status === AuthStatus.Unknown || isPending) {
+    return <div>Loading...</div>;
+  }
+
+  if (status === AuthStatus.Guest) {
+    return <Navigate to="/login" />;
+  }
+
   return <div className="flex flex-col gap-4">
     <Header/>
     <div className="min-h-screen"><Outlet/></div>
     <Footer/>
   </div>;
+};
+
+
+
+const useLogin = () => {
+  const {login, status} = useAuth();
+  const {data, isPending} = useFetchQuery("/auth/me");
+
+  useEffect(() => {
+    if (data) login(data);
+  }, [data]);
+
+  return {status, isPending};
 };
