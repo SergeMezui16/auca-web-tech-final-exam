565bbb3f95d3f666af59eef3c16766fae511cd47
sergemezui <sergemezui.dev@gmail.com>
2024-12-12 21:56:04 +0100

    Add SecurityConfiguration for authentication and JWT integration
    Introduce `SecurityConfiguration` to set up authentication and authorization. Configures password encoding, authentication manager, CSRF disabling, and a security filter chain using JWT for secure API access. This enhances application security with Spring Security.

---
+++ recipe-api/src/main/java/auca/recipe/configuration/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package auca.recipe.configuration;
+
+import auca.recipe.service.UserDetailService;
+import auca.recipe.utils.JWTUtil;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    private final UserDetailService userDetailService;
+    private final JWTUtil jwtUtil;
+
+    public SecurityConfiguration(UserDetailService userDetailService, JWTUtil jwtUtil) {
+        this.userDetailService = userDetailService;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, PasswordEncoder passwordEncoder) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailService).passwordEncoder(passwordEncoder);
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/register", "/auth/login").permitAll().anyRequest().authenticated())
+                .addFilterBefore(new JWTFilter(userDetailService, jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
