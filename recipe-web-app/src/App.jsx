f83bc0ec0b8dafca4d593773aa8ffbe2a4fb0b3f
sergemezui <sergemezui.dev@gmail.com>
2024-12-13 00:25:33 +0100

    Refactor App.jsx to handle authentication flow.
    Implemented conditional rendering based on authentication status. Added hooks to manage login state and integrated a login page for unauthenticated users. Refined routing by utilizing authenticated and public routers.

--- recipe-web-app/src/App.jsx
+++ recipe-web-app/src/App.jsx
@@ -1,8 +1,36 @@
+import { AuthStatus, useAuth } from "@/hooks/use-auth.js";
+import { RouterProvider } from "react-router";
+import { router, publicRouter } from "@/config/router.jsx";
+import { useFetchQuery, useMutation } from "@/hooks/use-queries.js";
+import { useEffect } from "react";
+import { LoginPage } from "@/pages/login.jsx";
+
 function App() {
+  const {status, isPending, isError} = useLogin()
+
+  if (status === AuthStatus.Unknown || isPending) {
+    return <div>Loading...</div>;
+  }
+
+  if (status === AuthStatus.Guest) {
+    return <LoginPage />;
+  }
 
   return (
-    <div className="text-center text-primary flex">Hello, world.</div>
+    <RouterProvider router={router}/>
   );
 }
 
 export default App;
+
+
+const useLogin = () => {
+  const {login, status} = useAuth();
+  const {data, isPending, isError} = useFetchQuery("/auth/me");
+
+  useEffect(() => {
+    if (data) login(data);
+  }, [data]);
+
+  return {status, isPending, isError};
+};
