95ac1b9407c4c17303784a21398ec0bf31b33de2
sergemezui <sergemezui.dev@gmail.com>
2024-12-12 21:55:08 +0100

    Add security and JWT dependencies to pom.xml
    Introduced Spring Security and JSON Web Token (JWT) libraries to the project. These additions include dependencies for security, testing, and JWT implementation using jjwt libraries. This setup lays the foundation for authentication and authorization functionality.

---
+++ recipe-api/src/main/java/auca/recipe/service/UserDetailService.java
@@ -0,0 +1,38 @@
+package auca.recipe.service;
+
+import auca.recipe.entity.User;
+import auca.recipe.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Component
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    public UserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(email);
+
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("User not found with email" + email);
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.get().getEmail(),
+                user.get().getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+    }
+}
