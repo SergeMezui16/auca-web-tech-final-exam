e50b1802cf4ab1f21cb46f67af5e4047200f563d
sergemezui <sergemezui.dev@gmail.com>
2024-12-12 21:58:26 +0100

    Add method to fetch rating by recipe and user
    Introduce `findOneByRecipeAndUser` in `RatingRepository` to streamline retrieval of a user's rating for a specific recipe. This enhances query efficiency and supports additional functionality dependent on this method.

--- recipe-api/src/main/java/auca/recipe/repository/RatingRepository.java
+++ recipe-api/src/main/java/auca/recipe/repository/RatingRepository.java
@@ -1,9 +1,13 @@
 package auca.recipe.repository;
 
 import auca.recipe.entity.Rating;
+import auca.recipe.entity.Recipe;
+import auca.recipe.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Long> {
+
+    Rating findOneByRecipeAndUser(Recipe recipe, User user);
 }
