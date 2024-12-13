3688580a425aafe781dd55674341a1f646af8e5f
sergemezui <sergemezui.dev@gmail.com>
2024-12-13 21:11:34 +0100

    Add pagination support to RecipeController
    Introduced a new `/paginate` endpoint to enable paginated and sorted retrieval of recipes, with optional filtering by name, description, and duration. Updated the default `/` endpoint to only return published recipes for better data handling. Removed unused `@JsonView` annotations for code simplification.

--- recipe-api/src/main/java/auca/recipe/controller/RecipeController.java
+++ recipe-api/src/main/java/auca/recipe/controller/RecipeController.java
@@ -6,9 +6,10 @@ import auca.recipe.entity.File;
 import auca.recipe.entity.Recipe;
 import auca.recipe.service.RecipeService;
 import auca.recipe.utils.AbstractApiController;
-import auca.recipe.view.RecipeViews;
-import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -27,31 +28,40 @@ public class RecipeController extends AbstractApiController {
     }
 
     @GetMapping
-    @JsonView(RecipeViews.Summary.class)
     public ResponseEntity<List<Recipe>> index() {
-        return this.send(this.service.repository.findAll());
+        return this.send(this.service.repository.findByIsPublished(true));
+    }
+
+    @GetMapping("/paginate")
+    public ResponseEntity<Page<Recipe>> paginate(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "true") boolean asc,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String description,
+            @RequestParam(required = false) Integer duration
+    ) {
+        Sort sort = asc ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        return this.send(this.service.paginate(PageRequest.of(page, size, sort), name, description, duration));
     }
 
     @PostMapping
-    @JsonView(RecipeViews.Summary.class)
     public ResponseEntity<?> create(@Valid @RequestBody RecipeDto dto) throws Exception {
         return this.send(this.service.create(dto));
     }
 
     @GetMapping("/{id}")
-    @JsonView(RecipeViews.Detailed.class)
     public ResponseEntity<?> show(@PathVariable Long id) {
         return this.send(this.service.repository.findById(id));
     }
 
     @PutMapping("/{id}")
-    @JsonView(RecipeViews.Summary.class)
     public ResponseEntity<?> edit(@Valid @RequestBody RecipeDto dto, @PathVariable Long id) {
         return this.send(this.service.update(id, dto));
     }
 
     @GetMapping("/{id}/publish")
-    @JsonView(RecipeViews.Summary.class)
     public ResponseEntity<?> publish(@PathVariable Long id) {
         return this.send(this.service.publish(id));
     }
@@ -71,7 +81,6 @@ public class RecipeController extends AbstractApiController {
     }
 
     @PostMapping("/{id}/rate")
-    @JsonView(RecipeViews.Summary.class)
     public ResponseEntity<?> rate(@PathVariable Long id, @Valid @RequestBody RateDto dto) throws Exception {
         return this.send(this.service.addRating(id, dto));
     }
