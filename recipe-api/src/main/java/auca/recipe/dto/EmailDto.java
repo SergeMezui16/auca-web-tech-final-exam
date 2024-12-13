e2544cf6140201cb56b0bc8b7e8900f30481bfc5
sergemezui <sergemezui.dev@gmail.com>
2024-12-13 12:32:37 +0100

    Add email sending functionality using EmailService
    Introduced `EmailDto` for email data transfer and `EmailService` for handling email sending logic. Included the necessary `spring-boot-starter-mail` dependency in `pom.xml` to support email functionality. This enables the application to send emails programmatically.

---
+++ recipe-api/src/main/java/auca/recipe/dto/EmailDto.java
@@ -0,0 +1,47 @@
+package auca.recipe.dto;
+
+public class EmailDto {
+
+    private String recipient;
+    private String body;
+    private String subject;
+    private String attachment;
+
+    public EmailDto(String recipient, String subject, String body) {
+        this.recipient = recipient;
+        this.subject = subject;
+        this.body = body;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(String attachment) {
+        this.attachment = attachment;
+    }
+}
---
+++ recipe-api/src/main/java/auca/recipe/service/EmailService.java
@@ -0,0 +1,40 @@
+package auca.recipe.service;
+
+import auca.recipe.dto.EmailDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String sender;
+
+    public EmailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+
+    public boolean send(EmailDto details) {
+        try {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+            mailMessage.setFrom(sender);
+            mailMessage.setTo(details.getRecipient());
+            mailMessage.setText(details.getBody());
+            mailMessage.setSubject(details.getSubject());
+
+            mailSender.send(mailMessage);
+            return true;
+        } catch (Exception e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+            return false;
+        }
+    }
+}
