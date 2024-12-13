d7b6bf60030f1a9ac236660a82566e911eba5954
sergemezui <sergemezui.dev@gmail.com>
2024-12-13 20:19:31 +0100

    Simplify AppLayout component by removing Header and Footer.
    This change eliminates Header and Footer components from AppLayout, leaving only the <Outlet />. Layout responsibilities can now be managed at a higher level, improving flexibility and reducing coupling.

--- recipe-web-app/src/components/organism/app-layout.jsx
+++ recipe-web-app/src/components/organism/app-layout.jsx
@@ -1,5 +1,3 @@
-import { Header } from "@/components/organism/header";
-import { Footer } from "@/components/organism/footer";
 import { Navigate, Outlet } from "react-router";
 import { AuthStatus, useAuth } from "@/hooks/use-auth.js";
 import { useFetchQuery } from "@/hooks/use-queries.js";
@@ -16,11 +14,7 @@ export const AppLayout = () => {
     return <Navigate to="/login" />;
   }
 
-  return <div className="flex flex-col gap-4">
-    <Header/>
-    <div className="min-h-screen"><Outlet/></div>
-    <Footer/>
-  </div>;
+  return <Outlet />;
 };
 
 
