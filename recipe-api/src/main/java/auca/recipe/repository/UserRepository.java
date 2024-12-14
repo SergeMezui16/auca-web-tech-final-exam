ee3e79cc437ae9b79b9e0fd23e19c750f47b7967
sergemezui <sergemezui.dev@gmail.com>
2024-12-14 11:38:57 +0100

    Add paginated search by name to UserRepository
    Introduced a method to find users by name with pagination support using `Pageable`. This enhances search functionality and improves scalability for large datasets.

--- recipe-api/src/main/java/auca/recipe/repository/UserRepository.java
+++ recipe-api/src/main/java/auca/recipe/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package auca.recipe.repository;
 
 import auca.recipe.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,8 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    Page<User> findByNameContaining(Pageable pageable, String name);
 
     Optional<User> findByEmail(String email);
 }
