a79165feb3ab737a9706bff02c8b92cd7232a623
sergemezui <sergemezui.dev@gmail.com>
2024-12-14 15:36:06 +0100

    Add MFA support to the login form
    This update introduces multi-factor authentication (MFA) functionality to enhance security during login. A new OTP step is triggered if the server indicates MFA is required, leveraging `InputNumber` for OTP input. Refactored the form logic to handle both standard login and MFA scenarios seamlessly.

--- recipe-web-app/src/components/forms/login-form.jsx
+++ recipe-web-app/src/components/forms/login-form.jsx
@@ -8,26 +8,57 @@ import {
 } from "@/components/ui";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
-import { InputText } from "@/components/atom/input.jsx";
+import { InputNumber, InputText } from "@/components/atom/input.jsx";
 import { extractServerErrors, useMutation } from "@/hooks/use-queries.js";
 import { useNavigate, Link } from "react-router";
 import { useAuth } from "@/hooks/use-auth.js";
+import { useState } from "react";
+
 
 export function LoginForm() {
   const navigate = useNavigate();
+  const [mfa, setMfa] = useState(false);
+  const [credentials, setCredentials] = useState({});
   const {login} = useAuth();
   const {register, setError, formState: {errors}, handleSubmit} = useForm();
+  const mfaForm = useForm();
   const {mutate, isPending} = useMutation("/auth/login", {}, "post", ["/auth/me"]);
+  const mfaQuery = useMutation("/auth/mfa", {}, "post", ["/auth/me"]);
 
   const handleLogin = (values) => {
     mutate(values, {
       onSuccess: (r) => {
-        login(r).then(() => navigate("/"));
-        toast.success("You have been logged in.");
+        login(r).then((v) => {
+          if(v === "MFA") {
+            setMfa(true);
+            setCredentials(values);
+          } else {
+            navigate("/");
+            toast.success("You have been logged in.");
+          }
+        })
       },
       onError: (error) => extractServerErrors(setError, error)
     });
   };
+
+  const handleOTP = (values) => {
+    mfaQuery.mutate(
+      {
+        ...credentials,
+        code: values.code
+      },
+      {
+        onSuccess: (r) => {
+          login(r).then((v) => {
+            navigate("/");
+            toast.success("You have been logged in.");
+          })
+        },
+        onError: (error) => extractServerErrors(mfaForm.setError, error)
+      }
+    )
+  }
 
   return (
     <Card className="mx-auto max-w-sm">
@@ -38,29 +69,45 @@ export function LoginForm() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form onSubmit={handleSubmit(handleLogin)} className="grid gap-4">
-          <InputText
+        {!mfa && <>
+          <form onSubmit={handleSubmit(handleLogin)} className="grid gap-4">
+            <InputText
+              disabled={isPending}
+              required
+              error={errors.email?.message}
+              type="email"
+              label="Email"
+              {...register("email")}
+              defaultValue={"serge@uii.com"}
+            />
+            <InputText
+              disabled={isPending}
+              required
+              error={errors.password?.message}
+              label="Password"
+              type="password"
+              {...register("password")}
+              defaultValue={"Pass56i?"}
+            />
+
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </form>
+        </>}
+        {mfa && <form onSubmit={mfaForm.handleSubmit(handleOTP)} className="grid gap-4">
+          <InputNumber
             disabled={isPending}
             required
-            error={errors.email?.message}
-            type="email"
-            label="Email"
-            {...register("email")}
-            defaultValue={"serge@uii.com"}
-          />
-          <InputText
-            disabled={isPending}
-            required
-            error={errors.password?.message}
-            label="Password"
-            type="password"
-            {...register("password")}
-            defaultValue={"Pass56i?"}
+            error={mfaForm.formState.errors?.code?.message}
+            label="Code OTP"
+            type="number"
+            {...mfaForm.register("code")}
           />
           <Button type="submit" className="w-full">
             Login
           </Button>
-        </form>
+        </form>}
         <Link to="/request_reset_password" className="block my-2 text-sm text-center w-full underline">
           Forgot your password?
         </Link>
